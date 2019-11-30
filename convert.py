40f4aa3a5b4908fdb991ed01284da41b97046aa6
benBlueKeller <benblue22@gmail.com>
2019-11-29 19:33:34 -0800

    refactored and added docstring

--- convert.py
+++ convert.py
@@ -4,14 +4,16 @@ from miditime.miditime import MIDITime
 data = [1,2,3,4,5,6,7]
 midi = MIDITime(120, 'midi.mid', 5, 5, 1)
 
-def to_notelist(data,
-            find_time = lambda ti: ti,
-            find_pitch = lambda pi: pi,
-            find_velocity = lambda ve=100: ve,
-            find_duration = lambda du=1: du):
+def to_notelist(data_list,
+                find_time=lambda ti: ti,
+                find_pitch=lambda pi: pi,
+                find_velocity=lambda ve=100: ve,
+                find_duration=lambda du=1: du):
+    """to_notelist takes a string of data and optional modifier functions for
+    time, pitch, velocity, and duration then returns a list of notes"""
     notelist = []
-    for point in data:
-        if type(point) == list:
+    for point in data_list:
+        if isinstance(point) == list:
             notelist.append([
                 find_time(point[0]),
                 find_pitch(point[1]) if len(point) > 1 else find_pitch(point[0]),
